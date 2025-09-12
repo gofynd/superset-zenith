cf921226cafe3e7d8c48f4710eed913ca40be35d
MarkTFKOP <markpereira199@gmail.com>
2025-09-12 12:33:31 +0530

    FIX: Custom styling for card components border-radius

--- superset-frontend/src/components/ListView/CardCollection.tsx
+++ superset-frontend/src/components/ListView/CardCollection.tsx
@@ -36,7 +36,6 @@ const CardContainer = styled.div<{ showThumbnails?: boolean }>`
     grid-gap: ${theme.gridUnit * 12}px ${theme.gridUnit * 4}px;
     grid-template-columns: repeat(auto-fit, 300px);
     justify-content: center;
-    border-radius: 4px !important;
     margin-top: ${theme.gridUnit * -6}px;
     padding: ${
       showThumbnails
