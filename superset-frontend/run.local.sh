502afcbcbe7700678fff550492349a078f794aae
MarkTFKOP <markpereira199@gmail.com>
2025-09-29 15:51:27 +0530

    ADD: prefix ZEN_ to SENTRY_8

--- superset-frontend/run.local.sh
+++ superset-frontend/run.local.sh
@@ -6,9 +6,9 @@ export BOLTIC_STREAMS_KEY=Zc7Og8DXevJG85xZyjKPJBAgkOITWUdtpDB8EsyQWxyx8JgqZIsGz-
 export BOLTIC_STREAMS_CHART_TRIGGER_THRESHOLDS=[{"chartId":152,"threshold":330000},{"chartId":248,"threshold":10000}]
 
 # Sentry Configuration
-export MJS_SENTRY_DSN=https://2b84b4e66780eedb23cac77ee8a579ac@o4510101081161728.ingest.us.sentry.io/4510101085093888
-export MJS_SENTRY_ENVIRONMENT=development
-export MJS_SENTRY_RELEASE=superset@4.1.3
+export ZEN_SENTRY_DSN=https://2b84b4e66780eedb23cac77ee8a579ac@o4510101081161728.ingest.us.sentry.io/4510101085093888
+export ZEN_SENTRY_ENVIRONMENT=development
+export ZEN_SENTRY_RELEASE=superset@4.1.3
 
 # Run dev server on custom host and port
 export ASSET_BASE_URL=/superset
