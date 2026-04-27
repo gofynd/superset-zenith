4d7eeec38c2d212e21ee3254e76d1d47683e026f
markpereira <markpereira@gofynd.com>
2026-04-27 15:44:57 +0530

    update: package lock

--- superset-frontend/run.local.sh
+++ superset-frontend/run.local.sh
@@ -1,2 +1,16 @@
-
 source env.sh && npm run dev-server -- --port=9001 --host=localdev.intelligence.fynd.com
+
+# source env.sh && npm run dev-server -- --port=9001 --host=localhost
+
+# source env.sh && \
+# SUPERSET_URL=$SUPERSET_URL \
+# npm run dev-server -- --port=9001 --host=0.0.0.0
+
+# source env.sh && SUPERSET_URL=https://superset.intelligence.fynd.com && npm run dev-server -- --port=9001 --host=0.0.0.0
+
+
+
+# RUN_MANIFEST_LOCAL=true \
+# SUPERSET=https://superset.intelligence.fynd.com \
+# DEV_SERVER_HOST=localdev.intelligence.fynd.com \
+# npm run dev-server -- --port=9001 --host=0.0.0.0
